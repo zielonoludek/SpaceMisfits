bf8ea2b927d2b01b5e2ddeb8109ed2a04f676b66
Michał Szcząchor <mike.programming265@gmail.com>
2025-02-25 13:12:51 +0100

    update events and created test scene

--- Assets/Scripts/ScriptableObjects/Events/SectorEventSO.cs
+++ Assets/Scripts/ScriptableObjects/Events/SectorEventSO.cs
@@ -3,9 +3,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SectorEventSO", menuName = "Events/New Event")]
 public class SectorEventSO : ScriptableObject
 {
-    public enum EventType { Story, Battle, Treasure, Spaceport}
-
-    public EventType eventType;
+    [Header("Event properties")]
     public string eventTitle;
-    [TextArea(3, 5)] public string eventDescription;
+    
+    public enum EventType { FaintSignal, Waypoint, DevilsMaw, SharpenThoseDirks}
+    public EventType eventType;
+    
+    [TextArea(3, 7)] public string eventDescription;
+
+    [SerializeField] private Effect eventEffect;
+
+    [Header("Choice-Based event settings")]
+    [SerializeField] private bool hasChoices;
+
+    [SerializeField] private string choice1Description;
+    [SerializeField] private Effect choice1Effect;
+    
+    [SerializeField] private string choice2Description;
+    [SerializeField] private Effect choice2Effect;
+
+    public string GetChoice1() => hasChoices ? choice1Description : null;
+    public string GetChoice2() => hasChoices ? choice2Description : null;
 }
