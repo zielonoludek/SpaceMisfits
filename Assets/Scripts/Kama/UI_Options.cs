5383b268d3fe437ad500f96b321146ca22fc9758
Kama <133801909+Kuzmiuk@users.noreply.github.com>
2025-03-04 10:21:38 +0100

    Graphis and Music

--- Assets/Scripts/Kama/UI_Options.cs
+++ Assets/Scripts/Kama/UI_Options.cs
@@ -1,3 +1,4 @@
+﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -26,8 +27,17 @@ public class UI_Options : MonoBehaviour
 
     private InputAction cancelAction;
 
+    private DropdownField resolutionDropdown;
+    private DropdownField qualityDropdown;
+    private Toggle fullscreenToggle;
+
     private AudioMixer audioMixer;
-    private Slider volumeSlider;
+
+    private Slider masterVolumeSlider;
+    private Slider sfxVolumeSlider;
+    private Slider musicVolumeSlider;
+
+    private Resolution[] resolutions;
 
 
 
@@ -53,12 +63,6 @@ public class UI_Options : MonoBehaviour
 
 
         optionsMenu = root.Q<VisualElement>("optionsMenu");
-
-        //if (mainMenu == null) Debug.LogError("MainMenuu nie ma ");
-        //if (pauseMenu == null) Debug.LogError("pauseMenu nie ma ");
-        //if (optionsMenu == null) Debug.LogError("optionsMenu nie ma ");
-
-
         optionsMenu.style.display = DisplayStyle.None;
 
 
@@ -70,24 +74,37 @@ public class UI_Options : MonoBehaviour
         cancelAction.performed += ctx => CloseOptions();
         cancelAction.Enable();
 
-        // VOLUME SLIDER AUDIO 
-        if (volumeSlider != null)
-        {
-            volumeSlider = root.Q<Slider>("VolumeSlider");
-            volumeSlider.RegisterValueChangedCallback(evt => SetVolume(evt.newValue));
-            LoadVolume();
-        }
-
         var resetButton = root.Q<Button>("ResetButton");
         resetButton.clicked += ResetSelectedRebinds;
 
         var allresetButton = root.Q<Button>("AllResetButton");
         allresetButton.clicked += AllResetRebinds;
 
-        rebindButton = root.Q<Button> ("RebindButton");
+        rebindButton = root.Q<Button>("RebindButton");
         rebindLabel = root.Q<Label>("RebindLabel");
         rebindButton.clicked += StartRebinding;
-        LoadRebinds();
+
+        resolutionDropdown = root.Q<DropdownField>("ResolutionDropdown");
+        qualityDropdown = root.Q<DropdownField>("FullscreenToggle");
+        fullscreenToggle = root.Q<Toggle>("QualityDropdown");
+
+        resolutionDropdown.RegisterValueChangedCallback(evt => SetResolution(evt.newValue));
+        fullscreenToggle.RegisterValueChangedCallback(evt => SetFullscreen(evt.newValue));
+        qualityDropdown.RegisterValueChangedCallback(evt => SetQuality(evt.newValue));
+
+        PopulateResolutionDropdown();
+        LoadGraphicsSettings();
+
+        // Audio
+        masterVolumeSlider = root.Q<Slider>("MasterVolumeSlider");
+        sfxVolumeSlider = root.Q<Slider>("SFXVolumeSlider");
+        musicVolumeSlider = root.Q<Slider>("MusicVolumeSlider");
+
+        masterVolumeSlider.RegisterValueChangedCallback(evt => SetMasterVolume(evt.newValue));
+        sfxVolumeSlider.RegisterValueChangedCallback(evt => SetSFXVolume(evt.newValue));
+        musicVolumeSlider.RegisterValueChangedCallback(evt => SetMusicVolume(evt.newValue));
+
+        LoadAudioSettings();
     }
 
     private void OnDisable()
@@ -139,23 +156,6 @@ public class UI_Options : MonoBehaviour
         }
 
     }
-
-    private void SetVolume(float volume)
-    {
-        audioMixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
-        PlayerPrefs.SetFloat("GameVolume", volume);
-    }
-
-    private void LoadVolume()
-    {
-        if (PlayerPrefs.HasKey("GameVolume"))
-        {
-            float savedVolume = PlayerPrefs.GetFloat("GameVolume");
-            audioMixer.SetFloat("Volume", Mathf.Log10(savedVolume) * 20);
-            volumeSlider.value = savedVolume;
-        }
-    }
-
     private void StartRebinding()
     {
         if(Application.platform != RuntimePlatform.WindowsPlayer &&
@@ -259,5 +259,86 @@ public class UI_Options : MonoBehaviour
         //LoadRebinds();
         UpdateRebindLabel();
     }
-    
+
+    private void PopulateResolutionDropdown()
+    {
+        Resolution[] resolutions = Screen.resolutions;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+                break;
+            }
+        }
+    }
+
+    private void SetResolution(string resolution)
+    {
+        string[] resParts = resolution.Split('x');
+        int width = int.Parse(resParts[0].Trim());
+        int height = int.Parse(resParts[1].Trim());
+
+        Screen.SetResolution(width, height, Screen.fullScreen);
+        PlayerPrefs.SetString("Resolution", resolution);
+    }
+
+    private void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+    }
+
+    private void SetQuality(string quality)
+    {
+        QualitySettings.SetQualityLevel(qualityDropdown.choices.IndexOf(quality));
+        PlayerPrefs.SetString("Quality", quality);
+    }
+
+    private void LoadGraphicsSettings()
+    {
+        if (PlayerPrefs.HasKey("Resolution"))
+            SetResolution(PlayerPrefs.GetString("Resolution"));
+
+        if (PlayerPrefs.HasKey("Fullscreen"))
+            fullscreenToggle.value = PlayerPrefs.GetInt("Fullscreen") == 1;
+
+        if (PlayerPrefs.HasKey("Quality"))
+            qualityDropdown.value = PlayerPrefs.GetString("Quality");
+    }
+
+    // Settings Volume 
+    private void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", volume);
+    }
+
+    private void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    private void LoadAudioSettings()
+    {
+        if (PlayerPrefs.HasKey("MasterVolume"))
+            masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+
+        if (PlayerPrefs.HasKey("SFXVolume"))
+            sfxVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        if (PlayerPrefs.HasKey("MusicVolume"))
+            musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+    }
+
 }
