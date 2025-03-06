46e07279e14a56dbd06f5ce2bdb5817b89074812
AntonioObeid <Antonio.Obeid@edu.futuregames.nu>
2025-03-06 15:27:12 +0200

    Main Game Screen + Speed + Food + Ship Health + Crew Morale

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -5,14 +5,20 @@ public class ResourceManager : MonoBehaviour
 {
     public static ResourceManager Instance { get; private set; }
 
+    private int shipHealth;
     private int booty;
     private int food;
     private int notoriety;
+    private int crewMorale;
     private int sightLevel;
+    private int speedValue;
 
+    public event Action<int> OnShipHealthChanged;
     public event Action<int> OnBootyChanged;
     public event Action<int> OnNotorietyChanged;
+    public event Action<int> OnCrewMoraleChanged;
     public event Action<int> OnSightChanged;
+    public event Action<int> OnSpeedChanged;
     public event Action<int> OnFoodChanged;
 
     private void Awake()
@@ -25,6 +31,19 @@ public class ResourceManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    public int ShipHealth
+    {
+        get => shipHealth;
+        set
+        {
+            if (shipHealth != value)
+            {
+                shipHealth = Mathf.Max(0, value);
+                OnShipHealthChanged?.Invoke(shipHealth);
+            }
         }
     }
 
@@ -50,6 +69,19 @@ public class ResourceManager : MonoBehaviour
             {
                 notoriety = Mathf.Max(0, value);
                 OnNotorietyChanged?.Invoke(notoriety);
+            }
+        }
+    }
+
+    public int CrewMorale
+    {
+        get => crewMorale;
+        set
+        {
+            if (crewMorale != value)
+            {
+                crewMorale = Mathf.Max(0, value);
+                OnCrewMoraleChanged?.Invoke(crewMorale);
             }
         }
     }
@@ -94,6 +126,31 @@ public class ResourceManager : MonoBehaviour
     public int GetCurrentSight()
     {
         return Sight;
+    }
+
+    public int Speed
+    {
+        get => speedValue;
+        set
+        {
+            speedValue = Mathf.Clamp(value, 0, 3);
+            OnSpeedChanged?.Invoke(speedValue);
+        }
+    }
+
+    public void IncreaseSpeed(int amount = 1)
+    {
+        Speed += amount;
+    }
+
+    public void DecreaseSpeed(int amount = 1)
+    {
+        Speed -= amount;
+    }
+
+    public int GetCurrentSpeed()
+    {
+        return Speed;
     }
 
     //ResourceManager.Instance.IncreaseSight(); //This line adds sight level in other scripts
