b4e4416f382fbf30b9dd7575ddb9735f678976c7
Kama <133801909+Kuzmiuk@users.noreply.github.com>
2025-03-14 09:35:54 +0100

    Canvas UI

---
+++ Assets/Devs/Kamila/Kama/Canvas/UI_MainMenu.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private InputActionAsset inputActions;
+
+    private UI_MainMenu ui_mainMenu;
+    private UI_Settings ui_Settings;
+
+    private InputAction clickAction;
+    private AudioSource audioSource;
+
+    private Button startButton;
+    private Button settingsButton;
+    private Button quitButton;
+    private List<Button> menuButtons = new List<Button>();
+
+    private void Awake()
+    {
+        ui_Settings = FindFirstObjectByType<UI_Settings>(FindObjectsInactive.Include);
+        ui_mainMenu = FindFirstObjectByType<UI_MainMenu>(FindObjectsInactive.Include);
+
+        mainMenu = GameObject.Find("UI_Main");
+
+        startButton = GameObject.Find("NewGame").GetComponent<Button>();
+        settingsButton = GameObject.Find("Settings").GetComponent<Button>();
+        quitButton = GameObject.Find("Quit").GetComponent<Button>();
+
+
+        audioSource = GetComponent<AudioSource>();
+
+        startButton.onClick.AddListener(() => SceneManager.LoadScene("SampleScene"));
+        settingsButton.onClick.AddListener(OpenOptions);
+        quitButton.onClick.AddListener(() => Application.Quit());
+
+        menuButtons.Add(startButton);
+        menuButtons.Add(settingsButton);
+        menuButtons.Add(quitButton);
+
+        foreach (var button in menuButtons)
+        {
+            button.onClick.AddListener(PlayClickSound);
+        }
+
+        var uiActionMap = inputActions.FindActionMap("UI");
+        clickAction = uiActionMap.FindAction("Click");
+        clickAction.performed += ctx => OnClickUI();
+        clickAction.Enable();
+
+        mainMenu.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        clickAction.Disable();
+    }
+
+    private void PlayClickSound()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
+    }
+
+    private void OnClickUI()
+    {
+        Debug.Log("Click");
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+    }
+
+    public void OpenOptions()
+    {
+        ui_Settings = FindFirstObjectByType<UI_Settings>(FindObjectsInactive.Include);
+        ui_Settings.OpenFromMainMenu();
+        mainMenu.SetActive(false);
+    }
+}
---
+++ Assets/Devs/Kamila/Kama/Canvas/UI_Pause.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI_Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private InputActionAsset inputActions;
+
+
+    private UI_Settings ui_Settings;
+    private UI_Pause ui_Pause;
+
+    private InputAction clickAction;
+    private InputAction pauseAction;
+
+    private Button backButton;
+    private Button settingsButton;
+    private Button quitButton;
+
+    public static bool isPaused = false;
+
+    private void Awake()
+    {
+        ui_Settings = FindFirstObjectByType<UI_Settings>(FindObjectsInactive.Include);
+        ui_Pause = FindFirstObjectByType<UI_Pause>(FindObjectsInactive.Include);
+
+        pauseMenu = GameObject.Find("UI_Pause");
+        backButton = GameObject.Find("BackToMain").GetComponent<Button>();
+        settingsButton = GameObject.Find("Settings").GetComponent<Button>();
+        quitButton = GameObject.Find("Quit").GetComponent<Button>();
+
+        
+        backButton.onClick.AddListener(() => SceneManager.LoadScene("MainMenu"));
+        settingsButton.onClick.AddListener(OpenOptions);
+        quitButton.onClick.AddListener(() => Application.Quit());
+
+        
+        var uiActionMap = inputActions.FindActionMap("UI");
+        clickAction = uiActionMap.FindAction("Click");
+        pauseAction = uiActionMap.FindAction("Cancel");
+
+        pauseAction.performed += ctx => Pause();
+
+        clickAction.Enable();
+
+        pauseMenu.SetActive(false);
+    }
+    public void Pause()
+    {
+        isPaused = !isPaused;
+        pauseMenu.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+        pauseAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        clickAction.Disable();    
+    }
+
+    public void ShowPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    public void OpenOptions()
+    {
+        if (ui_Settings == null)
+            ui_Settings = FindFirstObjectByType<UI_Settings>(FindObjectsInactive.Include);
+
+        ui_Settings.OpenFromPauseMenu();
+        pauseMenu.SetActive(false);
+        if (isPaused)
+        {
+            pauseAction.Disable();
+        }
+    }
+
+    public void PauseAction()
+    {
+        pauseAction.Enable();
+    }
+}
