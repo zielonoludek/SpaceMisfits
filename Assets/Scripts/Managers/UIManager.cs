900eb062399c4ff540da75e44389ec06005fa728
Michał Szcząchor <101410858+Mikehey265@users.noreply.github.com>
2025-03-17 15:54:03 +0100

    added event panel to UI manager (#41)
    * added event panel to UI manager
    
    * fixed conflicts with UI manager

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -5,8 +5,11 @@ public class UIManager : MonoBehaviour
     [SerializeField] TimePanelUI timePanelUI;
     [SerializeField] FightPanelUI fightPanelUI;
     [SerializeField] CrewRequestUI crewRequestUI;
+    [SerializeField] EventPopupUI eventPanelUI;
 
     public TimePanelUI TimePanelUI { get { return timePanelUI; } }
     public FightPanelUI FightPanelUI { get { return fightPanelUI; } }
     public CrewRequestUI CrewRequestUI { get { return crewRequestUI; } }
+
+    public EventPopupUI EventPanelUI { get { return eventPanelUI; } }
 }
