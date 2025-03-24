b4e20a2c3ff9ff12e17a5cd95b3c9b51271a893b
Michał Szcząchor <mike.programming265@gmail.com>
2025-03-24 17:07:09 +0100

    added hover value display on stats indicator UI

--- Assets/Devs/Michal/CircleStatusIndicator.cs
+++ Assets/Devs/Michal/CircleStatusIndicator.cs
@@ -1,12 +1,17 @@
-using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CircleStatusIndicator : MonoBehaviour
+public class CircleStatusIndicator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Image fillImage;
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TextMeshProUGUI valueText;
     [SerializeField] private float maxValue = 100f;
 
+    private float currentValue;
+    
     private void Start()
     {
         if (fillImage != null)
@@ -15,13 +20,51 @@ public class CircleStatusIndicator : MonoBehaviour
             fillImage.fillMethod = Image.FillMethod.Vertical;
             fillImage.fillOrigin = (int)Image.Origin360.Bottom;
         }
+
+        if (valueText != null)
+        {
+            valueText.gameObject.SetActive(false);
+        }
     }
     
-    public void UpdateFillAmount(float currentValue)
+    public void UpdateFillAmount(float value)
     {
+        currentValue = value;
+        
         if (fillImage != null)
         {
             fillImage.fillAmount = Mathf.Clamp01(currentValue / maxValue);
+        }
+
+        if (valueText != null)
+        {
+            valueText.text = Mathf.RoundToInt(value).ToString();
+        }
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (iconImage != null)
+        {
+            iconImage.gameObject.SetActive(false);
+        }
+
+        if (valueText != null)
+        {
+            valueText.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (iconImage != null)
+        {
+            iconImage.gameObject.SetActive(true);
+        }
+
+        if (valueText != null)
+        {
+            valueText.gameObject.SetActive(false);
         }
     }
 }
