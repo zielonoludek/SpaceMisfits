e55f5fbfb798082469dc5dbe4d9d4dadbb859e18
Michał Szcząchor <101410858+Mikehey265@users.noreply.github.com>
2025-03-25 15:39:53 +0100

    First implementation of stats UI (#51)
    * added event panel to UI manager
    
    * fixed conflicts with UI manager
    
    * added visualization to tied events
    
    * added placeholder status displays
    
    * added hover value display on stats indicator UI
    
    * added on hover tooltip for status UI

---
+++ Assets/Devs/Michal/TooltipManager.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+public class TooltipManager : MonoBehaviour
+{
+    [SerializeField] private GameObject tooltipPanel;
+    [SerializeField] private TextMeshProUGUI tooltipText;
+    [SerializeField] private float xOffset = 20f;
+    [SerializeField] private float yOffset = 0f;
+
+    private void Start()
+    {
+        HideTooltip();
+    }
+
+    public void ShowTooltip(string text, Vector2 position)
+    {
+        tooltipPanel.SetActive(true);
+        tooltipText.text = text;
+
+        // Calculate position to the right of the stat indicator
+        Vector2 newPosition = new Vector2(position.x + xOffset, position.y + yOffset);
+        tooltipPanel.transform.position = newPosition;
+    }
+
+    public void HideTooltip()
+    {
+        tooltipPanel.SetActive(false);
+    }
+}
