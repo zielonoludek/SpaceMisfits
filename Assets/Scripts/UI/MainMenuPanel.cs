e00033d7c1962207a90f77d6fd0a94f95adbfb70
Martyna Cichosz <martynacichosz2@gmail.com>
2025-03-27 17:28:46 +0100

    MainMenu

---
+++ Assets/Scripts/UI/MainMenuPanel.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenuPanel : MonoBehaviour
+{
+    [SerializeField] private Button startBtn;
+    [SerializeField] private Button continueBtn;
+    [SerializeField] private Button settingsBtn;
+    [SerializeField] private Button quitBtn;
+
+    void Start()
+    {
+        Setup();
+    }
+
+    private void Setup()
+    {
+        gameObject.SetActive(true);
+
+        startBtn.onClick.RemoveAllListeners();
+        settingsBtn.onClick.RemoveAllListeners();
+        quitBtn.onClick.RemoveAllListeners();
+        continueBtn.onClick.RemoveAllListeners();
+
+        startBtn.onClick.AddListener(() => SceneManager.LoadScene("MainGameScene"));
+        //settingsBtn.onClick.AddListener(OpenOptions);
+        quitBtn.onClick.AddListener(() =>
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        });
+
+    }
+}
